79c7da02ef7fe0e7dffbd3692aad30da7cf813fe
MisMOONMORE <2438138449@qq.com>
2022-01-25 17:22:05 +0800

    终于可以成功读取数据啦！

--- 读入文件.c
+++ 读入文件.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-void read_formated_text_by_struct(const char* packet1)
-{
+#include <stdlib.h>
+#define N 501
+/*void read_formated_text_by_struct(const char* packet1)
+	{
 	struct read packet;
 	FILE *file = fopen(C:\Users\Enolaliu\Desktop\第二次寒假作业资料\dataset\data\dataset1\packet1,"rt");
 	if(!file)read
@@ -14,4 +16,21 @@ void read_formated_text_by_struct(const char* packet1)
 		printf("source_ip:%s destination_ip:%s source_port:%d destination_port:%d protocol:%s\n",stu.source_ip,stu.destination_ip,stu.source_port,stu.destination_port,stu.protocol);		
 	}
 	fclose(file);
+}*/
+int main()
+{
+    FILE *fp;
+    char str[N+1];
+    if( (fp=fopen("c:\\Users\\Enolaliu\\Desktop\\winter2\\dataset\\data\\dataset1\\packet1.txt","rt")) == NULL )
+	{
+        puts("Fail to open file!");
+        exit(0);
+    }
+   
+    while(fgets(str, N, fp) != NULL){
+        printf("%s", str);
+    }
+
+    fclose(fp);
+    return 0;
 }
